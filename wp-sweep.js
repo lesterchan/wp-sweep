708cb063bdbdeb2d8e76efe1bea426c5c960b649
Julien Vernet <julien@vernet.me>
2015-03-02 11:51:39 +0700

    Fix and tidy up confirmation warning

--- wp-sweep.js
+++ wp-sweep.js
@@ -1,25 +1,31 @@
-jQuery(document).ready(function ($) {
+(function ($) {
+	'use strict';
 
 	var body = $('body'),
 		btn = $('[data-sweeping]');
 
-	/*
-	Prevent multiple sweeps at once
-	 */
-	btn.on('click', function (event) {
-		event.preventDefault();
-		$(this).addClass('disabled').text($(this).attr('data-sweeping'));
-		body.addClass('sweep-active');
+	$(function () {
+
+		/*
+		Prevent multiple sweeps at once
+		 */
+		btn.on('click', function () {
+			$(this).addClass('disabled').text($(this).attr('data-sweeping'));
+			setTimeout(function () {
+				body.addClass('sweep-active');
+			}, 50);
+		});
+
+		/*
+		Page closing confirmation
+		https://developer.mozilla.org/en-US/docs/DOM/Mozilla_event_reference/beforeunload
+		 */
+		$(window).on('beforeunload', function (e) {
+			if (body.hasClass('sweep-active')) {
+				return wpSweep.closeWarning;
+			}
+		});
+
 	});
 
-	/*
-	Page closing confirmation
-	https://developer.mozilla.org/en-US/docs/DOM/Mozilla_event_reference/beforeunload
-	 */
-	$(window).on('beforeunload', function (e) {
-		if (body.hasClass('sweep-active')) {
-			return wpSweep.closeWarning;
-		}
-	});
-
-});
+})(jQuery);
