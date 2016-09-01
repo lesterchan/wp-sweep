49423b25a30d0381ad3ef3e14a8eb59d49c4756c
Viktor Szépe <viktor@szepe.net>
2016-09-01 01:08:44 +0000

    Follow WPCS

--- class-command.php
+++ class-command.php
@@ -33,9 +33,9 @@ class WPSweep_Command extends WP_CLI_Command {
 	 *
 	 * ## EXAMPLES
 	 *
-	 *  1. wp sweep --all 
+	 *  1. wp sweep --all
 	 *		- Run Sweep for all the items.
-	 *  2. wp sweep revisions 
+	 *  2. wp sweep revisions
 	 *		- Sweep only Revision
 	 *  3. wp sweep revisions auto_drafts deleted_posts unapproved_comments spam_comments deleted_comments transient_options orphan_postmeta orphan_commentmeta orphan_usermeta orphan_termmeta orphan_term_relationships unused_terms duplicated_postmeta duplicated_commentmeta duplicated_usermeta duplicated_termmeta optimize_database oembed_postmet
 	 *		- Sweep the selected items
@@ -47,30 +47,30 @@ class WPSweep_Command extends WP_CLI_Command {
 		$items = array();
 
 		$default_items = array(
-			"0"  => 'revisions',
-			"1"  => 'auto_drafts',
-			"2"  => 'deleted_posts',
-			"3"  => 'unapproved_comments',
-			"4"  => 'spam_comments',
-			"5"  => 'deleted_comments',
-			"6"  => 'transient_options',
-			"7"  => 'orphan_postmeta',
-			"8"  => 'orphan_commentmeta',
-			"9"  => 'orphan_usermeta',
-			"10" => 'orphan_termmeta',
-			"11" => 'orphan_term_relationships',
-			"12" => 'unused_terms',
-			"13" => 'duplicated_postmeta',
-			"14" => 'duplicated_commentmeta',
-			"15" => 'duplicated_usermeta',
-			"16" => 'duplicated_termmeta',
-			"17" => 'optimize_database',
-			"18" => 'oembed_postmeta'
+			'0'  => 'revisions',
+			'1'  => 'auto_drafts',
+			'2'  => 'deleted_posts',
+			'3'  => 'unapproved_comments',
+			'4'  => 'spam_comments',
+			'5'  => 'deleted_comments',
+			'6'  => 'transient_options',
+			'7'  => 'orphan_postmeta',
+			'8'  => 'orphan_commentmeta',
+			'9'  => 'orphan_usermeta',
+			'10' => 'orphan_termmeta',
+			'11' => 'orphan_term_relationships',
+			'12' => 'unused_terms',
+			'13' => 'duplicated_postmeta',
+			'14' => 'duplicated_commentmeta',
+			'15' => 'duplicated_usermeta',
+			'16' => 'duplicated_termmeta',
+			'17' => 'optimize_database',
+			'18' => 'oembed_postmeta',
 		);
 
-		if ( isset( $assoc_args['all'] ) && $assoc_args['all'] == true ) {
+		if ( isset( $assoc_args['all'] ) && true == $assoc_args['all'] ) {
 			$this->run_sweep( $default_items );
-			WP_CLI::success( "Sweep Complete" );
+			WP_CLI::success( 'Sweep Complete' );
 
 			return;
 		} else {
@@ -81,7 +81,7 @@ class WPSweep_Command extends WP_CLI_Command {
 			}
 
 			$this->run_sweep( $items );
-			WP_CLI::success( "Sweep Complete!" );
+			WP_CLI::success( 'Sweep Complete!' );
 
 			return;
 		}
@@ -94,7 +94,7 @@ class WPSweep_Command extends WP_CLI_Command {
 
 		foreach ( $items as $key => $value ) {
 			$count = $sweep->count( $value );
-			if ( $count !== 0 && $count !== "0" ) {
+			if ( 0 !== $count && '0' !== $count ) {
 				$message = $sweep->sweep( $value );
 				WP_CLI::success( $message );
 			}
