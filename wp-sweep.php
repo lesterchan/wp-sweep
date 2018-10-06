20ea401abcb3d3a6cda18649444b91697819fd1a
Viktor Szépe <viktor@szepe.net>
2018-10-06 23:48:48 +0000

    Fix PHPStan findings

--- wp-sweep.php
+++ wp-sweep.php
@@ -43,7 +43,7 @@ define( 'WP_SWEEP_VERSION', '1.1.0' );
 /**
  * WP Rest API
  */
-require __DIR__ . '/class-wpsweep-api.php';
+require __DIR__ . '/inc/class-wpsweep-api.php';
 new WPSweep_Api();
 
 /**
@@ -68,7 +68,7 @@ class WPSweep {
 	 * @since 1.0.0
 	 *
 	 * @access private
-	 * @var $instance
+	 * @var WPSweep $instance
 	 */
 	private static $instance;
 
@@ -117,7 +117,7 @@ class WPSweep {
 	public function init() {
 		// include class for WP CLI command.
 		if ( defined( 'WP_CLI' ) ) {
-			require __DIR__ . '/class-wpsweep-command.php';
+			require __DIR__ . '/inc/class-wpsweep-command.php';
 			WP_CLI::add_command( 'sweep', 'WPSweep_Command' );
 		}
 	}
@@ -912,7 +912,7 @@ class WPSweep {
 	 */
 	public function plugin_activation( $network_wide ) {
 		if ( is_multisite() && $network_wide ) {
-			$ms_sites = get_sites();
+			$ms_sites = (array) get_sites();
 
 			if ( 0 < count( $ms_sites ) ) {
 				foreach ( $ms_sites as $ms_site ) {
@@ -948,7 +948,7 @@ class WPSweep {
 	 */
 	public function plugin_deactivation( $network_wide ) {
 		if ( is_multisite() && $network_wide ) {
-			$ms_sites = get_sites();
+			$ms_sites = (array) get_sites();
 
 			if ( 0 < count( $ms_sites ) ) {
 				foreach ( $ms_sites as $ms_site ) {
