5ed462fbab6a47e96260f440ffe0250308897046
Viktor Szépe <viktor@szepe.net>
2022-12-21 18:46:07 +0000

    Clean up main file's file comment
    Docs: https://developer.wordpress.org/plugins/plugin-basics/header-requirements/#header-fields

--- wp-sweep.php
+++ wp-sweep.php
@@ -1,37 +1,35 @@
 <?php
+
 /**
- * WP-Sweep wp-sweep.php
+ * WP-Sweep
  *
  * @package wp-sweep
+ *
+ * @wordpress-plugin
+ * Plugin Name: WP-Sweep
+ * Plugin URI: https://lesterchan.net/portfolio/programming/php/
+ * Description: WP-Sweep allows you to clean up unused, orphaned and duplicated data in your WordPress. It cleans up revisions, auto drafts, unapproved comments, spam comments, trashed comments, orphan post meta, orphan comment meta, orphan user meta, orphan term relationships, unused terms, duplicated post meta, duplicated comment meta, duplicated user meta and transient options. It also optimizes your database tables.
+ * Version: 1.1.5
+ * Author: Lester 'GaMerZ' Chan
+ * Author URI: https://lesterchan.net
+ * Text Domain: wp-sweep
+ * License: GPL2
+ *
+ *     Copyright 2022  Lester Chan  (email : lesterchan@gmail.com)
+ *
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License, version 2, as
+ *     published by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
-
-/*
-Plugin Name: WP-Sweep
-Plugin URI: https://lesterchan.net/portfolio/programming/php/
-Description: WP-Sweep allows you to clean up unused, orphaned and duplicated data in your WordPress. It cleans up revisions, auto drafts, unapproved comments, spam comments, trashed comments, orphan post meta, orphan comment meta, orphan user meta, orphan term relationships, unused terms, duplicated post meta, duplicated comment meta, duplicated user meta and transient options. It also optimizes your database tables.
-Version: 1.1.5
-Author: Lester 'GaMerZ' Chan
-Author URI: https://lesterchan.net
-Text Domain: wp-sweep
-License: GPL2
-*/
-
-/*
-	Copyright 2022  Lester Chan  (email : lesterchan@gmail.com)
-
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License, version 2, as
-	published by the Free Software Foundation.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
 
 /**
  * WP-Sweep version
