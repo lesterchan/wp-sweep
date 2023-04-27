642be4ff07bbd911306bf5fce465a1781f14ab24
Lester Chan <lesterchan@gmail.com>
2023-04-27 09:34:20 +0800

    Bump to WordPress 6.2

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ Contributors: GamerZ
 Donate link: https://lesterchan.net/site/donation/  
 Tags: sweep, clean, cleanup, clean up, optimize, orphan, unused, duplicated, posts, post meta, comments, comment meta, users, user meta, terms, term meta, term relationships, revisions, auto drafts, transient, database, tables, oembed
 Requires at least: 4.6  
-Tested up to: 6.1  
-Stable tag: 1.1.5  
+Tested up to: 6.2  
+Stable tag: 1.1.6  
 License: GPLv2 or later  
 License URI: http://www.gnu.org/licenses/gpl-2.0.html  
 
@@ -104,6 +104,10 @@ WP-Sweep is not compatible with the following plugins:
 I spent most of my free time creating, updating, maintaining and supporting these plugins, if you really love my plugins and could spare me a couple of bucks, I will really appreciate it. If not feel free to use it without any obligations.
 
 ## Changelog
+### 1.1.16
+* NEW: Re-org wp-sweep.php to inc/class-wpsweep.php
+* NEW: Bump to WordPress 6.2
+
 ### 1.1.15
 * NEW: Bump to WordPress 5.8
 
--- wp-sweep.php
+++ wp-sweep.php
@@ -9,13 +9,13 @@
  * Plugin Name: WP-Sweep
  * Plugin URI: https://lesterchan.net/portfolio/programming/php/
  * Description: WP-Sweep allows you to clean up unused, orphaned and duplicated data in your WordPress. It cleans up revisions, auto drafts, unapproved comments, spam comments, trashed comments, orphan post meta, orphan comment meta, orphan user meta, orphan term relationships, unused terms, duplicated post meta, duplicated comment meta, duplicated user meta and transient options. It also optimizes your database tables.
- * Version: 1.1.5
+ * Version: 1.1.6
  * Author: Lester 'GaMerZ' Chan
  * Author URI: https://lesterchan.net
  * Text Domain: wp-sweep
  * License: GPL2
  *
- *     Copyright 2022  Lester Chan  (email : lesterchan@gmail.com)
+ *     Copyright 2023  Lester Chan  (email : lesterchan@gmail.com)
  *
  *     This program is free software; you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License, version 2, as
@@ -36,7 +36,7 @@
  *
  * @since 1.0.0
  */
-define( 'WP_SWEEP_VERSION', '1.1.5' );
+define( 'WP_SWEEP_VERSION', '1.1.6' );
 
 require __DIR__ . '/inc/class-wpsweep.php';
 require __DIR__ . '/inc/class-wpsweep-api.php';
